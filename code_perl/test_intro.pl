4d0f8a2e202b0b203fdbad7159d1ec8c8d861af1
xie186 <xie186@purdue.edu>
2019-03-09 23:56:21 +0000

    Update

--- code_perl/test_intro.pl
+++ code_perl/test_intro.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 use warnings;
 use strict;
 ## this a random string
@@ -20,44 +20,31 @@ my $number = @gene_expr;
 #print "first element devided by 2 equals: ", $gene_expr[0]/2, "\n";
 #print "second element devided by 2 equals: ", $gene_expr[1]/2, "\n";
 
-# This is an example of  for loop 
+# This is an example of  for loop
 #for(my $i=0; $i<@gene_expr; ++$i){
 #    my $tem_var= $gene_expr[$i]/2;
 
     #print "At $i place, the number devided by 2 equals: $tem_var\n";
-    #print 'At $i place, the number devided by 2 equals: $tem_var\n';    
+    #print 'At $i place, the number devided by 2 equals: $tem_var\n';
 #}
 
-#my $j = 0;
-#foreach(@gene_expr){
-#    my $tem_var= $gene_expr[$j]/2;
-#    print "At $j place, the number devided by 2 equals: $tem_var\n";
-#    #$j = $j +1
-#    ++$j;
-#}
+
 
 #while
-#1, "string", 
+#1, "string",
 
 #print 3<2, "\n";
 #my $judge = 3>2;
 #print "Is 3>2? : $judge\n";
 #my $judge2 = 3<2;
 #print "Is 3<2? : xx..$judge2..xx\n";
-my $k = 0;
-while($k<@gene_expr){
-    #++$k;
-    my $tem_var= $gene_expr[$k]/2;
-    print "At $k place, the number devided by 2 equals: $tem_var\n";
-    ++$k;
-}
 
 
 #$k = 0;
 #while($k<@gene_expr){
 #    if($gene_expr[$k] >7){
 #        print "At $k place, the number is bigger than 7: $gene_expr[$k]\n";
-#    ## || 
+#    ## ||
 #    }elsif($gene_expr[$k] <=7 && $gene_expr[$k] >=5){
 #        print "At $k place, the number is between 5-7: $gene_expr[$k]\n";
 #    }else{
